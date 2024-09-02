a71775146e7e32ca3220a668c9bdc3affbf6a664
AlexDiGioia <digioia.alex2001@gmail.com>
2024-09-02 16:05:15 +0200

    Initial commit

---
+++ src/main/java/alex/s5l1Bend/S5l1BendApplication.java
@@ -0,0 +1,13 @@
+package alex.s5l1Bend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5l1BendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5l1BendApplication.class, args);
+	}
+
+}
